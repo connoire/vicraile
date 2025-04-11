d1915a9c3f085dd7135e76173b7c0a025eb48761
connoire <connoireeee@gmail.com>
2025-04-11 16:25:26 +1000

    made input box interactive with blinking cursor, added copy button to copy result to clipboard

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
+import pygame
+import sys
 import pandas as pd
 from geographiclib.geodesic import Geodesic
 import datetime
-
-import pygame
-import sys
+import pyperclip
 
 # function to compare two guesses
 def compare(guess, mystery, df, guesses):
@@ -12,9 +12,9 @@ def compare(guess, mystery, df, guesses):
     if mystery['Name'] == guess:
 
         if len(guesses) == 1:
-            msg = f'You got the correct station {guess} in 1 guess! Cheating?'
+            msg = f'You found the mystery station {guess} in 1 guess!'
         else:
-            msg = f'You got the correct station {guess} in {len(guesses)} guesses!'
+            msg = f'You found the mystery station {guess} in {len(guesses)} guesses!'
 
         return msg, True
     
@@ -55,7 +55,6 @@ def compare(guess, mystery, df, guesses):
 
 # initialise game
 pygame.init()
-pygame.key.set_repeat(300, 30)
 
 # setup display
 width, height = 650, 1000
@@ -66,16 +65,20 @@ pygame.display.update()
 # set fonts
 titlefont = pygame.font.SysFont('segoeuiemoji', 36)
 mainfont = pygame.font.SysFont('segoeuiemoji', 16)
+pygame.key.set_repeat(500, 30)
 
 # read data
 df = pd.read_parquet('stationdata.parquet')
 
 # pick mystery station
 seed = int(str(datetime.date.today()).replace('-', ''))
-mystery = df.sample(1).to_dict(orient = 'records')[0]
+mystery = df.sample(1, random_state = seed).to_dict(orient = 'records')[0]
 
 # initialise game states
 inputtext = ''
+inputactive = True
+cursortimer = 0
+cursorvisible = False
 guesses = []
 outputtext = []
 completed = False
@@ -85,6 +88,8 @@ cols = ['Guess', 'Distance', 'Patronage', 'Type', 'Line']
 pos = [30]
 for w in [160, 120, 120, 120, 60][:-1]:
     pos.append(pos[-1] + w + 10)
+copybutton = None
+copybuttonpressed = False
 
 # gameplay loop
 while True:
@@ -106,13 +111,43 @@ while True:
 
     for event in pygame.event.get():
 
+        # downclick
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            # input box
+            if 20 <= event.pos[0] and event.pos[0] <= 630 and 130 <= event.pos[1] and event.pos[1] <= 165 and not completed:
+                inputactive = True
+            else:
+                inputactive = False
+
+            # copy button
+            if completed and copybutton and copybutton.collidepoint(event.pos):
+                copybuttonpressed = True
+
+        # upclick
+        elif event.type == pygame.MOUSEBUTTONUP:
+            
+            # copy button
+            if completed and copybutton and copybutton.collidepoint(event.pos) and copybuttonpressed:
+
+                # create text
+                if len(guesses) == 1:
+                    text = f'🚇 VicRaile {datetime.date.today().strftime("%d/%m/%y")} 🚇\nI found the mystery station in 1 guess!\nhttp://upcomingwebsiteurl.yippee/'
+                else:
+                    text = f'🚇 VicRaile {datetime.date.today().strftime("%d/%m/%y")} 🚇\nI found the mystery station in {len(guesses)} guesses!\nhttp://upcomingwebsiteurl.yippee/'
+
+                # add to clipboard
+                pyperclip.copy(text)
+            
+            copybuttonpressed = False
+
         # quit game
-        if event.type == pygame.QUIT:
+        elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
         # keyboard input
-        elif event.type == pygame.KEYDOWN and not completed:
+        elif inputactive and event.type == pygame.KEYDOWN and not completed:
 
             # enter to make guess
             if event.key == pygame.K_RETURN:
@@ -151,19 +186,26 @@ while True:
     # render input box
     pygame.draw.rect(screen, (70, 70, 70), (20, 130, 610, 35), border_radius = 5)
     inputsurface = mainfont.render(f'Guess: {inputtext}', True, (255, 255, 255))
-    screen.blit(inputsurface, (30, 140))
+    screen.blit(inputsurface, (30, 141))
+
+    # blinking cursor
+    if inputactive and cursorvisible and not completed:
+        pygame.draw.line(screen, (255, 255, 255), (31 + inputsurface.get_width(), 139), (31 + inputsurface.get_width(), 155), 2)
+    if pygame.time.get_ticks() - cursortimer > 500:
+        cursorvisible = not cursorvisible
+        cursortimer = pygame.time.get_ticks()
 
     # popup for error inputs
     if popupmsg and pygame.time.get_ticks() < popuptimer:
         popupsurface = mainfont.render(popupmsg, True, (255, 80, 80))
-        screen.blit(popupsurface, ((width // 2) - popupsurface.get_width() // 2, 140))
+        screen.blit(popupsurface, ((width // 2) - popupsurface.get_width() // 2, 141))
 
     # render output table header
     if outputtext and '|' in outputtext[0]:
         for i, col in enumerate(cols):
-            tableheader = mainfont.render(col, True, (200, 200, 200))
+            tableheader = mainfont.render(col, True, (255, 255, 255))
             screen.blit(tableheader, (pos[i], 190))
-        pygame.draw.line(screen, (100, 100, 100), (30, 210), (620, 210), 1)
+        pygame.draw.line(screen, (255, 255, 255), (30, 210), (620, 210), 1)
 
     # render output table lines
     for i, line in enumerate(outputtext):
@@ -201,7 +243,21 @@ while True:
 
         # win message
         else:
+            
+            if len(guesses) == 1:
+                h = h - 30
+
             victorysurface = mainfont.render(line, True, (100, 255, 100))
             screen.blit(victorysurface, ((width // 2) - victorysurface.get_width() // 2, h))
+            
+            # copy button
+            copybutton = pygame.Rect(width // 2 - 50, h + 30, 100, 40)
+            if copybuttonpressed:
+                copybuttoncolour = (70, 100, 70)
+            else:
+                copybuttoncolour = (80, 120, 80)
+            pygame.draw.rect(screen, copybuttoncolour, copybutton, border_radius = 0)
+            buttonsurface = mainfont.render('Copy', True, (255, 255, 255))
+            screen.blit(buttonsurface, buttonsurface.get_rect(center = copybutton.center))
 
     pygame.display.flip()
